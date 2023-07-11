34c0ed4e32f757afd4e37d4ee61c2a5e6c5c53ac
Gilheim <Guy.vernon@mac.com>
2023-07-11 16:18:19 +0100

    made some more changes

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react'
-
+import { PokemonTable } from './components'
+import { PokemonForm } from './components'
 
 function App() {
-
+    const [inputText, setInputText] = useState('')
+    const [pokemon, setPokemon] = useState([])
 
     return (
         <>
-        <label >Search for a Pokémon</label>
-        <input type="text" placeholder='Pokémon'/>
-        <Pokemon />
+        <PokemonForm pokemon = {pokemon} setPokemon = {setPokemon} inputText = {inputText} setInputText = {setInputText} />
+        <PokemonTable pokemon = {pokemon} />
         </>
     )
 }
---
+++ pokemon/src/components/PokemonTable/index.jsx
@@ -0,0 +1,10 @@
+function PokemonTable(pokemon, setPokemon) {
+    if(pokemon.pokemon.length <1){
+        return(
+            <p>No Pokemon! Please search for one!</p>
+        )
+    }
+   
+}
+
+export default PokemonTable
